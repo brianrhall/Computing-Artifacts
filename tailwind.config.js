c380485566ac9fa4af1dfc30362c3954591767b3
Brian R. Hall <brianrhall@brianrhall.net>
2025-07-15 22:41:11 -0400

    updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      aspectRatio: {
+        'video': '3 / 4',  // Override aspect-video to be 3:4 instead of 16:9
+      }
+    },
   },
   plugins: [],
 }
